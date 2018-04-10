77f405734158c0e8224e7872d39ed26873514727
FangleiX Liu <fangleix.liu@intel.com>
2018-04-10 17:16:48 +0800

    add method

--- SECSDataLib/SECSDataLib/IData.cs
+++ SECSDataLib/SECSDataLib/IData.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace SECSDataLib
 {
-    class IData
+    public interface IData
     {
+        void WriteSecsByte(ref byte[] outArray, int arrayLen);
+        void WriteToSml(StringWriter writer);
     }
 }
