030fdf0a5e6c9725cc8866d5353dccc0d17ad54b
FangleiX Liu <fangleix.liu@intel.com>
2018-04-13 11:38:45 +0800

    tep

---
+++ SECSDataLib/SECSDataLib/DataItem/HSMSStreamParser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SECSDataLib.Core.DataItem
+{
+    public enum ParseStatus
+    {
+        PARSE_START,
+        PARSE_TOTLE_LEN,
+        PARSE_HSMS_HEADER,
+        PARSE_HSMS_BODY,
+        PARSE_END
+    }
+    public class HSMSStreamParser
+    {
+        public byte[] ReceiveBuffer { get; private set; }
+        public int WriteStart { get; private set; }
+        public int ReadStart { get; private set; }
+        public int BufferLength => ReceiveBuffer.Length - BufferStart;
+        public ParseStatus Status { get; private set; }
+    }
+}
